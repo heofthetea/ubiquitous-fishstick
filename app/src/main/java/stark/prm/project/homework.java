4a5e415cc93cb577246d55e15895256951a556cd
Josia Scheerle <scheerle.josia@gmail.com>
2024-06-27 17:04:41 +0200

    Update Layout and Move to dedicated activity

---
+++ app/src/main/java/stark/prm/project/homework.java
@@ -0,0 +1,37 @@
+package stark.prm.project;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class homework extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_homework);
+        createSpinnerElements();
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    //Dummy-Code TODO Change later
+    private void createSpinnerElements(){
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerLecture);
+        String[] arraySpinner = new String[] {"Analysis", "Algorithmen", "Projektmanagement", "Programmieren", "Web Engineering"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, arraySpinner);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+    }
+}
--- app/src/main/java/stark/prm/project/notes.java
+++ app/src/main/java/stark/prm/project/notes.java
@@ -7,18 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 
-
-public class MainActivity extends AppCompatActivity {
+public class notes extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_notes);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
