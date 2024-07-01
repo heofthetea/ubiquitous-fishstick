1b91ea77b8f77082299484e30137cd908d44ed5e
Josia Scheerle <46845152+ScheerleJo@users.noreply.github.com>
2024-07-01 11:52:16 +0200

    Add UI for Notes (#4)
    and Minor Fixes to existing UI

--- app/src/main/java/stark/prm/project/MainActivity.java
+++ app/src/main/java/stark/prm/project/MainActivity.java
@@ -1,17 +1,18 @@
 package stark.prm.project;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import stark.prm.project.data.Database;
 import stark.prm.project.data.Module;
+import stark.prm.project.uiHelper.UiSideMenu;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -28,20 +29,25 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
         Database.getInstance().add(
                 new Module("PRM", "Wolfgang Stark")
         );
-    //TODO code restart of App when run in Error
+        //TODO code restart of App when run in Error
         //Temporarily call the HomeworkActivity directly on Startup
         try {
-            this.startActivity(new Intent(MainActivity.this, HomeworkActivity.class));
+            //this.startActivity(new Intent(MainActivity.this, HomeworkActivity.class));
+
+            //Initial setup of SideBar-Navigation-Menu
+            UiSideMenu uiSideMenu = new UiSideMenu(this,findViewById(R.id.main));
+            Toolbar toolbar = findViewById(R.id.main_toolbar);
+            setSupportActionBar(toolbar);
+            uiSideMenu.handleSideMenu(findViewById(R.id.nav_main_view),toolbar, getSupportActionBar());
+
         } catch (Exception e) {
             Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
             //restart();
         }
-    }
-    public void restart() {
-        finish();
-        //this.startActivity(getIntent());
     }
 }
--- app/src/main/java/stark/prm/project/NoteActivity.java
+++ app/src/main/java/stark/prm/project/NoteActivity.java
@@ -1,12 +1,23 @@
 package stark.prm.project;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import stark.prm.project.data.Database;
+import stark.prm.project.data.Lecture;
+import stark.prm.project.data.Module;
+import stark.prm.project.data.Note;
+import stark.prm.project.uiHelper.UiSideMenu;
+import stark.prm.project.uiHelper.UiSpinner;
 
 public class NoteActivity extends AppCompatActivity {
 
@@ -15,10 +26,53 @@ public class NoteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_notes);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.note_drawer_layout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        setUpUiElements();
+        handleButton();
+    }
+
+    private void setUpUiElements(){
+        //Initial setup of Spinner Elements
+        Spinner modules = findViewById(R.id.note_spinner_module);
+        modules.setAdapter(new UiSpinner(this).createSpinnerElements());
+
+        //Initial setup of SideBar-Navigation-Menu
+        UiSideMenu uiSideMenu = new UiSideMenu(this,findViewById(R.id.note_drawer_layout));
+        Toolbar toolbar = findViewById(R.id.note_toolbar);
+        setSupportActionBar(toolbar);
+        uiSideMenu.handleSideMenu(findViewById(R.id.nav_notes_view),toolbar, getSupportActionBar());
+    }
+
+    private void handleButton(){
+        Button buttonAdd = findViewById(R.id.btn_add_note);
+        buttonAdd.setOnClickListener(view -> addNote());
+    }
+
+    private void addNote() {
+        Spinner spinner =  findViewById(R.id.note_spinner_module);
+        EditText editLecture = findViewById(R.id.note_edit_text_lecture);
+        String topic = editLecture.getText().toString();
+        EditText editDesc = findViewById(R.id.note_edit_text_description);
+        Database db = Database.getInstance();
+
+        //Get Object of the Module selected by the user
+        Module module = db.getModuleByName(spinner.getSelectedItem().toString());
+        if(module == null) throw new RuntimeException("selected Module doesn't exist");
+
+        //Get Object for the Lecture if existent, otherwise create a new Lecture object
+        Lecture lecture = db.getLectureByTopic(topic);
+        if(lecture == null) {
+            lecture = new Lecture(module, topic);
+            db.add(lecture);
+        }
+
+        db.add(new Note(
+                editDesc.getText().toString(),
+                lecture
+        ));
     }
 }
